4060c8cd856e95fd4329d4f83e7c5b64866d5bf1
Jack <jacksweikert@gmail.com>
2026-08-30 12:08:39 -0400

    changelog

--- app.js
+++ app.js
@@ -14551,6 +14551,7 @@ function setupChangelog(){
     safe=safe.replace(/`([^`]+)`/g,'<code>$1</code>');
     safe=safe.replace(/\*\*([^*]+)\*\*/g,'<strong>$1</strong>');
     safe=safe.replace(/\*([^*]+)\*/g,'<em>$1</em>');
+    safe=safe.replace(/!\[([^\]]*)\]\((https?:\/\/[^)\s]+)\)/g,'<img class="changelog-image" src="$2" alt="$1" loading="lazy" decoding="async">');
     safe=safe.replace(/\[([^\]]+)\]\((https?:\/\/[^)\s]+)\)/g,'<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>');
     return safe;
   };
